31ed811bf1f7b7cc5205758eee65f6be23902570
shawern@students.cs.ubc.ca <shawern101@gmail.com>
2026-05-01 16:15:33 -0700

    made blog root path

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     #include() chops off url part that has mached up to that point
-    #only sends the remaining string to blog urls, in this case sends empty string
-    path('blog/', include('blog.urls')), #blog.urls is just a regular inport meaning blogs/urls.py
+    #only sends the remaining string to blog urls, in this case full blogs/url
+    path('', include('blog.urls')), #blog.urls is just a regular inport meaning blogs/urls.py
 ]
