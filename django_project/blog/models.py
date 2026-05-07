757846836129ea9d1db2d745d51f21587bebed42
shawern@students.cs.ubc.ca <shawern101@gmail.com>
2026-05-06 17:21:56 -0700

    added comments

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -8,5 +8,8 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE) #one to many relationship
 
+    #python dunder method, it controls what happens when you do str(obj)/print(obj)
+    #dunder means double underscore method (also called magic methods or special methods)
+    #This makes it so that Django Post.objects.all() shows each object like <Post: Blog 1> instead of <Post: Post object (1)>.
     def __str__(self):
         return self.title
